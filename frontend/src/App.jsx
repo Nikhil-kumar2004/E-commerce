61b98986720d8a3cff640dab0dcaf7fe7689ac6b
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-04 15:12:21 +0530

    resolved asset file bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,13 @@ import Login from './pages/Login'
 import Orders from './pages/Orders'
 import PlaceOrder from './pages/PlaceOrder'
 import Product from './pages/Product'
+import NavBar from './components/NavBar'
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
