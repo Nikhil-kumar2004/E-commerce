f7e6dccbb0c5c6d3253059ac5d720bfb52257911
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-05 16:20:52 +0530

    latestCollections page completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../components/hero'
+import LatestCollections from '../components/LatestCollections'
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <LatestCollections/>
     </div>
   )
 }
