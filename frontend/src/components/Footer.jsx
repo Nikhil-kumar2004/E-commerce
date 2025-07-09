a565c56b0296980f080495c860e6433dd55c6979
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-09 18:08:44 +0530

    About,Contact and Login done, frontend completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div>
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-25 text-sm'>
             <div>
                 <img src={assets.logo} className='mb-5 w-32' alt="" />
                 <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum minima, magni exercitationem repellendus corrupti cumque numquam dolorem sed consequuntur eaque?</p>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const Contact = () => {
   return (
     <div>
-      
+        <div className='text-2xl text-center pt-10 border-t'>
+            <Title text1={'CONTACT'} text2={'US'} />
+        </div>
+        <div className='my-8 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+            <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+            <div className='flex flex-col justify-center gap-6 items-start'>
+              <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+              <p className='text-gray-500'> Clothify Enterprices <br /> 139, block-59, Delhi-100234</p>
+              <p className='text-gray-500'>Tel: (+91) 123456789 <br />Email: admin@clothify.com</p>
+              <p className='font-semibold text-xl text-gray-600'>Careers at Clothify</p>
+              <p className='text-gray-500'>Learn more about our teams and job opening.</p>
+              <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-200'>Explore Jobs</button>
+            </div>
+        </div>
+        <NewsLetterBox/>
     </div>
   )
 }
