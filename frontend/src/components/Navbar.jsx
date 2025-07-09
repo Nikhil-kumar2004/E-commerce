496b63d7c0f1fd3903a4a4ecfa3858ab01b84a68
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-09 15:08:42 +0530

    orders page is done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
         <div className='flex items-center gap-4'>
             <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
             <div className='group relative'>
-                <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+                <Link to={'/login'}><img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" /></Link>
                 <div className='group-hover:block absolute hidden dropdown-menu hover right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-4 bg-stone-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
