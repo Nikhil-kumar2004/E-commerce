95b3b037d63d43ad9ffc0aba9c1897b51ba609a7
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-13 18:36:03 +0530

    adminAuth done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,7 +76,21 @@ const registerUser= async (req, res)=>{
 }
 
 const adminLogin= async (req, res)=>{
+    try{
+        const {email, password}=req.body
 
+        if(email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
+            const token=createToken(email+password)
+            res.json({success:true, token})
+        }
+        else{
+            res.json({success:false, msg:"Invalid Credentials"})
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false, msg:error.message})
+    }
 }
 
 export {loginUser, registerUser, adminLogin};
