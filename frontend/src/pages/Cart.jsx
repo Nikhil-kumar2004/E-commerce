c9f4349327b84f671c4447cb1877aeae120e3617
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-23 13:57:06 +0530

    error resolve 4

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
-import CartTotal from './components/CartTotal';
+import CartTotal from '../components/CartTotal';
 import { toast } from 'react-toastify';
 
 const Cart = () => {
