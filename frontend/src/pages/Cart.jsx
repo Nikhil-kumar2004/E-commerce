5d66bc962b66307320aa150df721954f4df788b8
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-23 13:54:52 +0530

    error resolved 3

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,9 +2,8 @@ import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
-import CartTotal from '../components/cartTotal';
+import CartTotal from './components/CartTotal';
 import { toast } from 'react-toastify';
-import axios from 'axios';
 
 const Cart = () => {
 
