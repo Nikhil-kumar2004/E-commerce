55f25e2275d7ab3e66f75f9f202d9163934d61e0
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-25 13:20:22 +0530

    my profile removed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -49,7 +49,6 @@ const NavBar = () => {
                     {
                         token && <div className='group-hover:block absolute hidden dropdown-menu hover right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-4 bg-stone-100 text-gray-500 rounded'>
-                            <p className='cursor-pointer hover:text-black'>My Profile</p>
                             <p onClick={()=>navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
                             <p onClick={()=>logout()} className='cursor-pointer hover:text-black'>Logout</p>
                         </div>
