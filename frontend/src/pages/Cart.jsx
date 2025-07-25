1911a601812dd79d2a9521cd50ea65654633a4a4
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-07-25 15:08:08 +0530

    cart updated

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -7,20 +7,20 @@ import { toast } from 'react-toastify';
 
 const Cart = () => {
 
-  const {currency, products, cartItems, updateCartQuantity, navigate, deleteFromCart}=useContext(ShopContext);
-  const [cartData, setCartData]=useState([]);
+  const { currency, products, cartItems, updateCartQuantity, navigate } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    if(products.length > 0){
-      let tempData=[];
-      for(const items in cartItems){
-        for(const item in cartItems[items]){
-          if(cartItems[items][item] > 0){
+    if (products.length > 0) {
+      let tempData = [];
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
             tempData.push({
-              _id:items,
-              size:item,
-              quantity:cartItems[items][item]
+              _id: items,
+              size: item,
+              quantity: cartItems[items][item]
             })
           }
         }
@@ -28,17 +28,17 @@ const Cart = () => {
       setCartData(tempData);
     }
 
-  },[cartItems, products])
+  }, [cartItems, products])
 
-  return (
+  return (cartData > 0) ? (
     <div className='border-t pt-14'>
       <div className='text-2xl mb-3'>
         <Title text1={'YOUR'} text2={'CART'} />
       </div>
       <div>
         {
-          cartData.map((item, index)=>{
-            const productData=products.find((products)=>products._id===item._id)
+          cartData.map((item, index) => {
+            const productData = products.find((products) => products._id === item._id)
 
             return (
               <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
@@ -52,8 +52,8 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
-                <input onChange={(e)=>updateCartQuantity(item._id,item.size,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
-                <img onClick={()=>updateCartQuantity(item._id,item.size,0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
+                <input onChange={(e) => updateCartQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+                <img onClick={() => updateCartQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
               </div>
             )
           })
@@ -62,14 +62,18 @@ const Cart = () => {
 
       <div className='flex justify-end my-20'>
         <div className='w-full sm:w-[450px]'>
-          <CartTotal/>
+          <CartTotal />
           <div className='w-full text-end'>
-            <button onClick={()=>(cartData.length > 0) ? navigate('/place-order') : toast.error('PLEASE ADD ITEM')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+            <button onClick={() => (cartData.length > 0) ? navigate('/place-order') : toast.error('PLEASE ADD ITEM')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
           </div>
         </div>
       </div>
     </div>
-  )
+  ) : <div className="flex items-center justify-center h-[40vh]">
+    <h1 className="text-3xl md:text-5xl text-gray-800 font-serif">
+      Cart is Empty!
+    </h1>
+  </div>
 }
 
 export default Cart
