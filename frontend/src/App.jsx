5869da83f5410bef8e39ec6114a5003d83c72d20
Nikhil Kumar <nikhilkr1022004@gmail.com>
2025-08-20 17:30:36 +0530

    lazy loading added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,47 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Cart from './pages/Cart'
-import Collection from './pages/Collection'
-import Contact from './pages/Contact'
-import Login from './pages/Login'
-import Orders from './pages/Orders'
-import PlaceOrder from './pages/PlaceOrder'
-import Product from './pages/Product'
-import NavBar from './components/Navbar'
-import Footer from './components/Footer'
-import { ToastContainer} from 'react-toastify';
-import Verify from './pages/Verify'
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
+import { ToastContainer } from 'react-toastify';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+//less critical pages
+const About = lazy(() => import('./pages/About'));
+const Cart = lazy(() => import('./pages/Cart'));
+const Collection = lazy(() => import('./pages/Collection'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Orders = lazy(() => import('./pages/Orders'));
+const PlaceOrder = lazy(() => import('./pages/PlaceOrder'));
+const Product = lazy(() => import('./pages/Product'));
+const Verify = lazy(() => import('./pages/Verify'));
 
 const App = () => {
   return (
-    <div className='px-4 lg:px-[3vw]'>
+    <div className="px-4 lg:px-[3vw]">
       <ToastContainer />
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/collection' element={<Collection/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/orders' element={<Orders/>} />
-        <Route path='/place-order' element={<PlaceOrder/>} />
-        <Route path='/product/:productId' element={<Product/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/verify' element={<Verify/>} />
-      </Routes>
-      <Footer/>
-    </div>
-  )
-}
+      <NavBar />
 
-export default App
+      <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />        {/* eager load */}
+          <Route path="/login" element={<Login />} />  {/* eager load */}
+          
+          <Route path="/about" element={<About />} />  {/* lazy */}
+          <Route path="/cart" element={<Cart />} />    {/* lazy */}
+          <Route path="/collection" element={<Collection />} /> {/* lazy */}
+          <Route path="/orders" element={<Orders />} />  {/* lazy */}
+          <Route path="/place-order" element={<PlaceOrder />} /> {/* lazy */}
+          <Route path="/product/:productId" element={<Product />} /> {/* lazy */}
+          <Route path="/contact" element={<Contact />} /> {/* lazy */}
+          <Route path="/verify" element={<Verify />} />   {/* lazy */}
+        </Routes>
+      </Suspense>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
